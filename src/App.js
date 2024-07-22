a2ab3573623d0bd7c30310045f01b1c2df28cae1
dhoumaima <dhoumaima077@gmail.com>
2024-07-22 11:54:28 +0100

    App design

--- src/App.js
+++ src/App.js
@@ -1,37 +1,46 @@
 import './App.css';
-
+import React, { useState } from 'react';
 function App() {
+  const [isCompleteScreen,setIsCompleteScreen] = useState(false);
   return (
+    
     <div className="App">
       <h1>My to do </h1>
       <div className='todo-wrapper'>
+
         <div className='todo-input'>
-          <div>
+          <div className="todo-input-item">
             <label>Title</label>
             <input type="text" placeholder="What's your task title ?"></input>
           </div>
-        </div>
-        <div className='todo-input'>
-          <div>
+
+
+          <div className="todo-input-item">
             <label>Description</label>
             <input type="text" placeholder="your task detail"></input>
           </div>
-        </div>
-        <div className='todo-input'>
-          <div>
+
+
+          <div className="todo-input-item">
             <button type="button" className="primaryBtn">Add</button>
           </div>
-        </div>
+          </div>
+
+
         <div className="btn-area">
-          <button>to Do</button>
-          <button>Completed</button>
+          <button className={`secondaryBtn ${isCompleteScreen===false && 'active'}`} onClick={()=>setIsCompleteScreen(false)}>to Do</button>
+          <button className={`secondaryBtn ${isCompleteScreen===true && 'active'}`} onClick={()=>setIsCompleteScreen(true)} >Completed</button>
         </div>
+
+
         <div className="todo-list">
           <div className="todo-list-item">
           <h3>task1</h3>
           <p>Description</p>
           </div>
         </div>
+
+
       </div>
     </div>
   );
