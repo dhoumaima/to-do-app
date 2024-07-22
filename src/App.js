55a3cfc050a277aeadec60fcc2ccc685634a07f5
dhoumaima <dhoumaima077@gmail.com>
2024-07-22 14:23:04 +0100

    Add Button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { MdDelete } from "react-icons/md";
+import { BsCheckLg } from "react-icons/bs";
 function App() {
   const [isCompleteScreen,setIsCompleteScreen] = useState(false);
+  const [allTodos,setTodos] = useState([]);
+  const [newTitle,setNewTitle] = useState("");
+  const [newDescription,setNewDescription] = useState("");
+
+
+  const handleAssTodo = ()=>{
+    let newTodoItem = {
+      title:newTitle,
+      description:newDescription,
+    };
+    let updateToDoArr=[...allTodos];
+    updateToDoArr.push(newTodoItem);
+    setTodos (updateToDoArr);
+    localStorage.setItem('todolist',JSON.stringify(updateToDoArr))
+  };
+   
+
+  useEffect(()=>
+  {
+    let savedTodo = JSON.parse(localStorage.getItem('todolist'));
+    if (savedTodo)
+    {
+      setTodos(savedTodo);
+
+    }
+  },[])
   return (
     
     <div className="App">
@@ -11,33 +39,48 @@ function App() {
         <div className='todo-input'>
           <div className="todo-input-item">
             <label>Title</label>
-            <input type="text" placeholder="What's your task title ?"></input>
+            <input type="text" value={newTitle} onChange={(e)=>setNewTitle(e.target.value)} placeholder="What's your task title ?"></input>
           </div>
 
 
           <div className="todo-input-item">
             <label>Description</label>
-            <input type="text" placeholder="your task detail"></input>
+            <input type="text" value={newDescription} onChange={(e)=>setNewDescription(e.target.value)} placeholder="your task detail"></input>
           </div>
 
 
           <div className="todo-input-item">
-            <button type="button" className="primaryBtn">Add</button>
+            <button type="button" onClick={handleAssTodo} className="primaryBtn">Add</button>
           </div>
           </div>
 
 
         <div className="btn-area">
-          <button className={`secondaryBtn ${isCompleteScreen===false && 'active'}`} onClick={()=>setIsCompleteScreen(false)}>to Do</button>
-          <button className={`secondaryBtn ${isCompleteScreen===true && 'active'}`} onClick={()=>setIsCompleteScreen(true)} >Completed</button>
+          <button className={`secondaryBtn ${isCompleteScreen===false && 'active'}`} onClick={()=>setIsCompleteScreen(false)}>
+            To Do
+          </button>
+          <button className={`secondaryBtn ${isCompleteScreen===true && 'active'}`} onClick={()=>setIsCompleteScreen(true)} >
+            Completed
+          </button>
         </div>
 
 
         <div className="todo-list">
-          <div className="todo-list-item">
-          <h3>task1</h3>
-          <p>Description</p>
-          </div>
+          {allTodos.map((item,index)=>{
+            return(
+            <div className="todo-list-item" key={index}>
+              <div>
+                <h3>{item.title}</h3>
+                <p>{item.description}</p>
+              </div>
+              <div>
+                <MdDelete  className='icon' title='Delete?'/>
+                <BsCheckLg className='check-icon' title='Complete?'/>
+              </div>
+            </div>
+            )
+          })}
+          
         </div>
 
 
